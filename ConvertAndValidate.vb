0a983af5b66e5a8afe02be13bf1c66f2f91fbed4
callkend <77285430+callkend@users.noreply.github.com>
2021-02-01 08:08:33 -0700

    Add project files.

---
+++ ConvertAndValidate.vb
@@ -0,0 +1,39 @@
+﻿'Kendall Callister
+'RCET0265
+'Spring 2021
+'ConvertAndValidate
+
+Option Explicit On
+Option Strict On
+
+Module ConvertAndValidate
+    Sub Main()
+        Dim userInput As String
+        Dim validated As String
+        Dim convertedInteger As Integer
+        Console.WriteLine("Enter String to be converted to a Integer")
+        userInput = Console.ReadLine()
+        validated = ValidateAndConvert(userInput, convertedInteger)
+        If validated = String.Empty Then
+            Console.WriteLine($"Integer = {convertedInteger}")
+        Else
+            Console.WriteLine($"User Input {validated}")
+        End If
+        Console.ReadLine()
+    End Sub
+
+    Function ValidateAndConvert(convertThisString As String, ByRef toThisInterger As Integer) As String
+        Try
+            toThisInterger = CInt(convertThisString)
+            Return String.Empty
+        Catch ex As Exception
+            If convertThisString = "" Then
+                Return "is empty"
+            Else
+                Return "must contain number"
+            End If
+
+        End Try
+    End Function
+
+End Module
